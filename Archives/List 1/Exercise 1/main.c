79c444c45a3c4e04a8bd5970e33d616a4eeb45b8
Neemias Renan <neemiasrenan2015@gmail.com>
2024-02-22 12:45:41 -0300

    Atualização de Lista

---
+++ Archives/List 1/Exercise 1/main.c
@@ -0,0 +1,16 @@
+// 1 O programa a seguir escreve uma mensagem de Olá mundo! no terminal de
+// comandos em que é executado:
+// #include <stdio.h>
+//
+// int main(){
+//      printf("Olá mundo!\n");
+//      return 0;
+// }
+// Escreva, compile e execute o programa. Para compilar, execute:
+
+#include <stdio.h>
+int main()
+{
+    printf("Olá mundo!\n");
+    return 0;
+}
